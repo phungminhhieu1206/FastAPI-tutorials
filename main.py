1e28ab74ba064548d8dbc03b7e3bb77570f2a7f1
Phung Minh Hieu <phungminhhieu1206@gmail.com>
2022-10-11 11:22:43 +0700

    basic concepts - path parameters

--- main.py
+++ main.py
@@ -5,4 +5,26 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data': {'name': 'Phung Minh Hieu'}}
+    return {'data': 'list blogs'}
+
+
+# static router need write above dynamic router
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'unpublished blogs'}
+
+
+# dynamic router
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    # fetch comments of blog with id = id
+    return {'data': {
+        'blog_id': id,
+        'comments': 'list comments'
+    }}
