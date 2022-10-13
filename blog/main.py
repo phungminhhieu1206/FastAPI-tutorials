74c0a24c5e1cf4143a0818a6bc08526712d3aa05
Phung Minh Hieu <phungminhhieu1206@gmail.com>
2022-10-13 23:17:20 +0700

    get all records, get records by id from database

--- blog/main.py
+++ blog/main.py
@@ -25,3 +25,15 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blogs')
+def get_blog(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blogs/{id}')
+def get_blog_by_id(id, db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).filter(models.Blog.id == id).all()
+    return blogs
